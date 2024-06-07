f0d53936e42c969fa2b20dd960d816e6b1d4129c
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-07 22:10:46 +0200

    *

--- src/core/arm/emitter.rs
+++ src/core/arm/emitter.rs
@@ -1,1 +1,11 @@
-pub struct ArmEmitter {}
+pub struct ARMEmitter {}
+
+impl ARMEmitter {
+  pub fn new() -> Self {
+    Self {}
+  }
+
+  pub fn emit(&self) {
+    todo!("Emit ARM Instruction");
+  }
+}
