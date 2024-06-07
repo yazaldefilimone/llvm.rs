475a7cd135ac144529aabc1b9837dcb47631b59a
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-07 22:10:46 +0200

    *

--- src/utils/symbol_table.rs
+++ src/utils/symbol_table.rs
@@ -1,1 +1,15 @@
 pub struct SymbolTable {}
+
+impl SymbolTable {
+  pub fn new() -> Self {
+    Self {}
+  }
+
+  pub fn add(&self) {
+    todo!("Add Symbol");
+  }
+
+  pub fn get(&self) {
+    todo!("Get Symbol");
+  }
+}
