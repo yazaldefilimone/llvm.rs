edae062ff15cc149ae7ddd24acac7b47e56c6e88
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-07 22:10:46 +0200

    *

--- src/utils/type_manager.rs
+++ src/utils/type_manager.rs
@@ -1,1 +1,15 @@
 pub struct TypeManager {}
+
+impl TypeManager {
+  pub fn new() -> Self {
+    Self {}
+  }
+
+  pub fn add(&self) {
+    todo!("Add Type");
+  }
+
+  pub fn get(&self) {
+    todo!("Get Type");
+  }
+}
