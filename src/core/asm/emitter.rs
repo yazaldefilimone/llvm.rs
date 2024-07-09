91092d60776e6d5a2dd920c0c6a29467a2f75260
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-09 10:54:38 +0200

    *

---
+++ src/core/asm/emitter.rs
@@ -0,0 +1,13 @@
+// assembly emitter
+
+pub struct ASMEmitter {}
+
+impl ASMEmitter {
+  pub fn new() -> Self {
+    Self {}
+  }
+
+  pub fn emit(&self) {
+    todo!("Emit Assembly Instruction");
+  }
+}
